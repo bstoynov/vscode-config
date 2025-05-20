9b9489e32896f9cc61d8dca8a8cec6c64c3b3565
Bogomil Stoynov <bogomil@bstoynov.com>
2025-05-20 21:03:02 +0300

    change init.lua

--- init.lua
+++ init.lua
@@ -14,3 +14,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.opt.ignorecase = true
 -- UNLESS \C or one or more capital letters in the search term
 vim.opt.smartcase = true
+
+-- Increase memory to fix crashes inside VSCode
+-- vim.opt.maxmempattern = 2000
+
+-- No Highlight when seraching
+vim.o.hlsearch = false
+
+-- Disable Cmd + D in insert mode to allow for multiple cursors in vscode
+vim.api.nvim_set_keymap('i', '<D-d>', '', { noremap = true, silent = true })
+
+-- Remap W to w because when saving I don't let out shift quick enough
+vim.api.nvim_create_user_command("W", "w", {})
